d1472b5cba694e526168e6b45953e1c5e3a39f28
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-02 18:47:59 -0500

    Auto-commit: Actualización de código

---
+++ 07-funciones/js/03-ejercicio.js
@@ -0,0 +1,12 @@
+function calcular_circunferencia (diametro) {
+    
+return Math.PI * diametro
+
+}
+
+function mostar_resulatdo (){
+let diametro = document.getElementById("diametro");
+let resultado = document.getElementById("resultado_mensaje");
+let opreacion = calcular_circunferencia(diametro.value);
+resultado.textContent = opreacion;
+}
