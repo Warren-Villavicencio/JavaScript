ea73a616052959bfaca0acdc1cbf90c709bd1f8e
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-03 11:05:50 -0500

    Auto-commit: Actualización de código

--- 07-funciones/js/04-ejercicio.js
+++ 07-funciones/js/04-ejercicio.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let numero_aleatorio = document.getElementById("mostrar_resultado");
-
-    function mostrar_numero_aleatorio() {
-        numero_aleatorio.textContent = Math.random();
-    }
-
-    document.querySelector("button").onclick = mostrar_numero_aleatorio;
-});
+<script src="js/04-ejercicio.js"></script>
