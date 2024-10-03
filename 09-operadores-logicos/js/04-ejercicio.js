2bf80289e51fbd232a0050e6e3161f4544380a30
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-03 18:04:52 -0500

    Auto-commit: Actualización de código

--- 09-operadores-logicos/js/04-ejercicio.js
+++ 09-operadores-logicos/js/04-ejercicio.js
@@ -1,5 +1,5 @@
 function validar_edad() {
-let edad = document.getElementById();
+let edad = document.getElementById("");
 let mensaje = edad >= 18 ? (edad >= 65 ? "Jubilado" : "Adulto") : "Menor de edad";
 console.log(mensaje); // Output: "Adulto"
 }
