60717869177b8fd76091565fc929e053d15a24ea
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-03 11:06:02 -0500

    Auto-commit: Actualización de código

--- 07-funciones/js/04-ejercicio.js
+++ 07-funciones/js/04-ejercicio.js
@@ -1,1 +1,14 @@
-<script src="js/04-ejercicio.js"></script>
+// Espera a que el DOM esté completamente cargado antes de ejecutar el código
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtiene el elemento con el id "mostrar_resultado" y lo asigna a la variable numero_aleatorio
+    let numero_aleatorio = document.getElementById("mostrar_resultado");
+
+    // Define la función mostrar_numero_aleatorio
+    // Esta función genera un número aleatorio y lo muestra en el elemento numero_aleatorio
+    function mostrar_numero_aleatorio() {
+        numero_aleatorio.textContent = Math.random();
+    }
+
+    // Asigna la función mostrar_numero_aleatorio al evento onclick del botón
+    document.querySelector("button").onclick = mostrar_numero_aleatorio;
+});
