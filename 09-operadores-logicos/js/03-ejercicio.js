1e694bfc7dce4ba8836ab6793c30890dc89d8df3
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-03 12:48:54 -0500

    Auto-commit: Actualización de código

--- 09-operadores-logicos/js/03-ejercicio.js
+++ 09-operadores-logicos/js/03-ejercicio.js
@@ -2,7 +2,7 @@
 function validar_conexion () {
 
 let estaConectado = document.getElementById("estaConectado").value.toLowerCase() === "si"; // Convertir a minúsculas y comparar con "sí";
-
+let resultado = document.getElementById("resultado_mensaje");
 if (!estaConectado) {
     resultado.textContent = ("Debes conectarte a internet");
 }
