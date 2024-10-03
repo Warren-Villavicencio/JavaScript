1eda99db3dfe892c13ebb84eec6bedd0b8de3d26
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-03 11:19:33 -0500

    Auto-commit: Actualización de código

--- 07-funciones/js/04-ejercicio.js
+++ 07-funciones/js/04-ejercicio.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Define la función mostrar_numero_aleatorio
     // Esta función genera un número aleatorio y lo muestra en el elemento numero_aleatorio
-    function mostrar_numero_aleatorio(maximo) {
+    function mostrar_numero_aleatorio() {
         
         numero_aleatorio.textContent = Math.floor(Math.random());
     }
