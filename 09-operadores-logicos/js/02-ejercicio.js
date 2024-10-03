1c998bff21d81852ae5884a3804239a7051bccc7
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-03 12:09:52 -0500

    Auto-commit: Actualización de código

--- 09-operadores-logicos/js/02-ejercicio.js
+++ 09-operadores-logicos/js/02-ejercicio.js
@@ -4,6 +4,7 @@
         let estaLloviendo = document.getElementById("estaLloviendo");
 function validar_clima () {
         if (esDia || !estaLloviendo) {
+            
             alert("Podemos salir a caminar");
         }
 
