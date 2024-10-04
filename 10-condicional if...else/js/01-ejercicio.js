4ecf572575102b5c6735bb13b3c9ce6bdd663dc1
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-04 02:11:18 -0500

    Auto-commit: Actualización de código

--- 10-condicional if...else/js/01-ejercicio.js
+++ 10-condicional if...else/js/01-ejercicio.js
@@ -33,7 +33,7 @@ function simularClima() {
   } else if (temperatura >= 10 && temperatura < 20) {
 
     mostrar_recomendaciones.innerHTML = `
-    <p>"¡Hace mucho calor y humedad! Usa ropa ligera y bebe mucha agua. Evita actividades al aire libre en las horas pico."</p>
+    <p>"El clima es fresco. Usa una chaqueta ligera y disfruta de un paseo al aire libre."</p>
     
 `;
 
