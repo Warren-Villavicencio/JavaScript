19badd8167107b9152ea7cda75a091c88a009154
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-04 01:53:39 -0500

    Auto-commit: Actualización de código

--- 10-condicional if...else/js/01-ejercicio.js
+++ 10-condicional if...else/js/01-ejercicio.js
@@ -4,7 +4,7 @@ function simularClima() {
     const humedad = Math.floor(Math.random() * 100) + 1; // Entre 1 y 100%
     const probabilidadLluvia = Math.floor(Math.random() * 100) + 1; // Entre 1 y 100%
   
-    let mostrar_resultado = document.getElementById("mostrar_resultado")
+    let mostrar_resultado = document.getElementById("mostrar_resultado");
     mostrar_resultado.textContent = ("Temperatura:", temperatura, "°C");
     console.log("Temperatura:", temperatura, "°C");
     console.log("Humedad:", humedad, "%");
