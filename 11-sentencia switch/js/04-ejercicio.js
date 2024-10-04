8a370a6c4cec2b67c09b56dc72a8504589d13f31
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-04 14:55:17 -0500

    Auto-commit: Actualización de código

--- 11-sentencia switch/js/04-ejercicio.js
+++ 11-sentencia switch/js/04-ejercicio.js
@@ -1,7 +1,7 @@
 function menu () {
 
     let  numero =  document.getElementById("numero").value;
-    let menu = numero.;
+    let menu = numero.value;
     let mostrar_resultado = document.getElementById("mostrar_resultado");
     switch (menu) {
 
