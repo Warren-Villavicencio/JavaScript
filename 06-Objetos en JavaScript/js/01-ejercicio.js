43b40e8b4b7ff639e478bccdf8f972ba03a4e8d4
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-09 00:20:12 -0500

    Auto-commit: Actualización de código

---
+++ 06-Objetos en JavaScript/js/01-ejercicio.js
@@ -0,0 +1,11 @@
+let vaca = {
+    Número_identificación:"0001",
+    Raza:"holstein",
+    Sexo:"femenino",
+    Fecha_nacimiento:"15/5/2021",
+    Edad:"5",
+
+    Registrar_producción_leche () {
+        console.log("produccion de leche 20 litros")
+    }
+};
