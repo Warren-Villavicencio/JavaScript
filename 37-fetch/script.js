8adfd5a2f462c87429dcce60d2b985c67ee6b498
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-10-15 09:30:13 -0500

    Auto-commit: Actualización de código

---
+++ 37-fetch/script.js
@@ -0,0 +1,17 @@
+document.getElementById('fetchDogButton').addEventListener('click', fetchDogImage);
+
+function fetchDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById('dogImage').src = data.message;
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la solicitud Fetch:', error);
+        });
+}
